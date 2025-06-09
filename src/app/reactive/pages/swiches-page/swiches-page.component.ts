54a581b1968ece1209e43e0801d204520279508a
= <=>
2025-06-09 11:55:58 +0200

    Switches, radioButtons and checkBox

--- src/app/reactive/pages/swiches-page/swiches-page.component.ts
+++ src/app/reactive/pages/swiches-page/swiches-page.component.ts
@@ -1,9 +1,30 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormsUtils } from '../../../utils/forms.utils';
 
 @Component({
   selector: 'app-swiches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './swiches-page.component.html',
 })
-export class SwichesPageComponent {}
+export class SwichesPageComponent {
+  private fb = inject(FormBuilder);
+  formsUtils = FormsUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', [Validators.required]],
+    wantNotifications: [true],
+    termsAndConditions: [false, [Validators.requiredTrue]],
+  });
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+}
